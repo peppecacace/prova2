6f8add708da7238cc5a781d34104733e254de799
peppeuchiha <giuseppe.cacace2@studenti.unicampania.it>
2018-06-21 23:30:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/peppe/prova/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.peppe.prova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}///////////////////////////
